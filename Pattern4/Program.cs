1aa7e47f64c00d8e9746fdbfedaad12447a423a4
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-01-31 23:49:16 -0600

    Solution to pattern 4

---
+++ Pattern4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Pattern4 {
+    class Program {
+        static void PrintRow(int i) {
+            for (int j = 1; j <= i; j++) {
+                Console.Write(j);
+            }
+        }
+
+        static void Main(string[] args) {
+            int num=0;
+            while (true) {
+                Console.Write("Enter a number: ");
+                if (!int.TryParse(Console.ReadLine(),out num)) {
+                    continue;
+                }
+
+                for (int i = num ; i >= 1; i--) {
+                    PrintRow(i);
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                for (int i = 1; i <= num; i++) {
+                    PrintRow(i);
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
