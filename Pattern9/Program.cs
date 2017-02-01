c7f8d67e3a6cd66343ce6b33a0722f9a0e605e04
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-01 00:37:58 -0600

    Solution to pattern 9

---
+++ Pattern9/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+namespace Pattern9 {
+    class Program {
+        static void Main(string[] args) {
+            int n = 5;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j <= i; j++) {
+                    Write(n - j);
+                }
+                WriteLine();
+            }
+        }
+    }
+}
