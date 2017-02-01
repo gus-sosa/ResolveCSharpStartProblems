795104c7700e160bfc293eece93a98080b428d59
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-01-31 23:30:41 -0600

    Solution to Pattern1
    http://www.csharpstar.com/10-different-number-pattern-programs-in-csharp/

---
+++ Pattern1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pattern1 {
+    class Program {
+        static void PrintPattern(int num) {
+            for (int row = 1; row <= num; row++) {
+                string cadStart = "", cadEnd = "";
+                for (int col = 1; col < row; col++) {
+                    cadStart += col;
+                    cadEnd = cadEnd.Insert(0, "" + col);
+                }
+                var padding = new string(' ', num - row);
+                Console.WriteLine($"{padding}{cadStart}{row}{cadEnd}{padding}");
+            }
+        }
+
+        static void Main(string[] args) {
+            int num = 0;
+            while (true) {
+                Console.Write("Enter a number between 1 to 9: ");
+                if (int.TryParse(Console.ReadLine(), out num) && num >= 1 && num <= 9) {
+                    PrintPattern(num);
+                }
+            }
+        }
+    }
+}
