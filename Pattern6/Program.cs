418ea4283185dc010406c415c90cb55fa5558585
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-01-31 23:57:34 -0600

    Solution to pattern6

---
+++ Pattern6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern6 {
+    class Program {
+        static void Main(string[] args) {
+            int num = 0;
+            while (true) {
+                Console.Write("Enter the number of rows: ");
+                if (!int.TryParse(Console.ReadLine(),out num) && num>=1) {
+                    continue;
+                }
+
+                int lastNum = 1;
+                for (int i = 1; i <= num; i++) {
+                    for (int j = 0; j < i; j++) {
+                        Console.Write($"{lastNum++} ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
