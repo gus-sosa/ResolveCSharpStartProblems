db358320365475244a3355e4ef882d69cf6487fe
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-01-31 23:52:23 -0600

    Solution to pattern 5

---
+++ Pattern5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern5 {
+    class Program {
+        static void Main(string[] args) {
+            int num = 0;
+            while (true) {
+                Console.Write("Enter the number of rows: ");
+                if (!int.TryParse(Console.ReadLine(), out num)) {
+                    continue;
+                }
+
+                bool flag = true;
+                for (int i = 0; i < num; i++) {
+                    for (int j = 0; j <= i; j++) {
+                        Console.Write(flag ? "1" : "0");
+                        flag = !flag;
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
