46b4446cecd0c10ab5317efbcbbe78d1f6f92688
Gustavo Sosa Cuervo <g.sosacuervo@hotmail.com>
2017-02-01 00:42:18 -0600

    Solution to pattern 10

---
+++ Pattern10/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+namespace Pattern10 {
+    class Program {
+        static void Main(string[] args) {
+            int n = 9;
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= i; j++) {
+                    Write(j);
+                }
+                WriteLine(new string('*', n - i));
+            }
+        }
+    }
+}
